4c1e80a5364493fd41aa0091e1edd16523d8a58e
shanmuk <shanmukha.sai.nunna@accenture.com>
2024-06-24 15:58:57 +0530

    removed test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-runs_per_over = int(input("What is the run rate per over :"))
-
-total_overs = int(input("What is the total overs of the match :"))
-
-total_runs = (runs_per_over * total_overs)
-print("Total runs of the match: ", total_runs)
-print("Thank you")
-print("awesome")
